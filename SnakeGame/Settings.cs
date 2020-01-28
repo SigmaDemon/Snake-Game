41dc26f8d0821b5d89e8d0e51cc7d731540cf675
Sebastian Iulian Poiana <sebastianpoiana2015@gmail.com>
2020-01-28 19:54:02 +0000

    A Snake Game developed in C# to eat as much food appearing on the screen and making the snake grow bigger! Fun!

---
+++ SnakeGame/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    // This declaration provides the movement of the snake when playing
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    class Settings
+    {
+        // These are the variables for the game settings declared before being used
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static int Speed { get; set; }
+        public static int Score { get; set; }
+        public static int Points { get; set; }
+        public static bool GameOver { get; set; }
+        public static Direction direction { get; set; }
+        public static Direction Direction { get; internal set; }
+
+        // This method will provide the global settings for the game
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Speed = 16;
+            Score = 0;
+            Points = 100;
+            GameOver = false;
+            direction = Direction.Down;
+
+        }
+    }
+}
